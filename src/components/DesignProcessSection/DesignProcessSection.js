985c418b56a5f346bf19dd0fddcf9b5b535ed46c
TvickyPace <tvicky.pacefin@gmail.com>
2024-12-29 14:56:28 +0530

    feat: desgin process section is done

--- src/components/DesignProcessSection/DesignProcessSection.js
+++ src/components/DesignProcessSection/DesignProcessSection.js
@@ -1,4 +1,5 @@
 import { aeonikTrial, layGrotesk } from "@/app/font";
+import Image from "next/image";
 import React from "react";
 
 function DesignProcessSection() {
@@ -29,7 +30,65 @@ function DesignProcessSection() {
                 Design & Refine
               </p>
             </div>
-            <div className="w-full bg-white"></div>
+            <div className="flex w-full flex-col gap-y-[27px] rounded-[16px] border-[1px] border-white p-[25px]">
+              <p
+                className={`${aeonikTrial.className} text-[24px] uppercase leading-[32px] tracking-[0.03em]`}
+              >
+                Strong foundations
+              </p>
+              <div className="flex flex-col gap-y-[22px]">
+                <div className="flex gap-x-[10px]">
+                  <Image
+                    src={"/goalIcon.svg"}
+                    alt="goal icon"
+                    width={22}
+                    height={22}
+                    className="aspect-square w-[22px]"
+                  />
+                  <p
+                    className={`${aeonikTrial.className} text-[19.375px] leading-[24px]`}
+                  >
+                    Goal
+                  </p>
+                </div>
+                <div className="flex flex-col gap-y-[27px]">
+                  <p
+                    className={`${aeonikTrial.className} text-[22px] font-light leading-[24px]`}
+                  >
+                    Understand the project’s goals and user needs, Clarify the
+                    challenges and define the scope for a tailored design
+                    solution
+                  </p>
+                  <div className="flex flex-wrap gap-x-[14px] gap-y-[20px]">
+                    {[
+                      "Client Discussion",
+                      "Problem Identification",
+                      "Defining Core Objectives",
+                    ]?.map((item, index) => {
+                      return (
+                        <div
+                          key={index}
+                          className="bg-white px-[8px] py-[5px] text-center"
+                        >
+                          <p
+                            className={`${aeonikTrial.className} text-[12px] uppercase leading-[16px] text-black`}
+                          >
+                            {item}
+                          </p>
+                        </div>
+                      );
+                    })}
+                  </div>
+                  <div className="rounded-[20px] bg-white/10 p-[24px]">
+                    <p
+                      className={`${aeonikTrial.className} text-[22px] font-light italic leading-[22px]`}
+                    >
+                      “The more you know, the better you design”
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
