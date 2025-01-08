e92f118ab4246261aa4c58cd8962c60b1694b9e6
TvickyPace <tvicky.pacefin@gmail.com>
2025-01-08 23:55:25 +0530

    feat: noting done

--- src/components/VisualDesignSection/VisualDesignSection.js
+++ src/components/VisualDesignSection/VisualDesignSection.js
@@ -5,7 +5,17 @@ import React from "react";
 function VisualDesignSection() {
   return (
     <div className="relative h-screen">
-      <div className="absolute left-1/2 top-calc_50p_160px -translate-x-1/2 -translate-y-1/2">
+      {/* grid */}
+      <div className="absolute left-0 top-0 flex h-full w-full justify-center gap-x-[323px]">
+        {Array(5)
+          .fill(true)
+          .map((_, index) => {
+            return (
+              <div key={index} className={`border-x-[0.5px] border-white/15`} />
+            );
+          })}
+      </div>
+      <div className="absolute left-1/2 top-calc_50p_160px z-10 -translate-x-1/2 -translate-y-1/2">
         <p className="flex flex-col text-nowrap text-center text-[64px] leading-[80px] tracking-[0.04em]">
           <span>
             I pair strong{" "}
@@ -34,7 +44,7 @@ function VisualDesignSection() {
         alt="magical hand"
         width={426}
         height={396.5}
-        className="absolute bottom-0 right-0 aspect-auto w-[426px]"
+        className="absolute bottom-0 right-0 z-10 aspect-auto w-[426px]"
       />
     </div>
   );
