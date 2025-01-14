a4a27a69d7ee9d3447200cc013ee1818995483ec
TvickyPace <tvicky.pacefin@gmail.com>
2025-01-15 01:24:54 +0530

    Feat: wireframe section is done

--- src/components/WireFrameSection/WireFrameSection.js
+++ src/components/WireFrameSection/WireFrameSection.js
@@ -1,9 +1,25 @@
 "use client";
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import GridLines from "../UtilitiesComponents/GridLines";
 
 function WireFrameSection() {
-  const wireFrameSection = useRef();
+  const [mouseDownActive, setMouseDownActive] = useState(false);
+  const [cursorPositionPercentage, setCursorPositionPercentage] = useState(0.5);
+
+  const wireFrameSectionRef = useRef();
+
+  // Dragging cursor
+  function draggingCursor(event) {
+    if (!mouseDownActive) return;
+    const wireFrameSectionRect =
+      wireFrameSectionRef.current.getBoundingClientRect();
+    const cursorX = event.clientX - wireFrameSectionRect.left;
+    const percentage = Number(cursorX / wireFrameSectionRect.width);
+    setCursorPositionPercentage(
+      Number(Math.max(0.025, Math.min(0.975, percentage))),
+    );
+  }
+
   return (
     <>
       <div className="flex h-screen flex-col">
@@ -12,35 +28,53 @@ function WireFrameSection() {
         </div>
         <div
           style={{ backgroundImage: `url(/skillsBg.svg)` }}
-          className="h-full border-y-2 border-white/15 bg-auto bg-clip-padding bg-left-top bg-repeat px-[72px]"
+          className="h-[90%] overflow-hidden border-y-2 border-white/15 bg-auto bg-clip-padding bg-left-top bg-repeat px-[72px]"
         >
           <div className="outline-x-2 relative h-full w-full overflow-hidden bg-black outline-white/15">
-            <div className="relative flex h-full w-full flex-nowrap">
+            <div
+              ref={wireFrameSectionRef}
+              onMouseUpCapture={() => {
+                setMouseDownActive(false);
+              }}
+              onMouseMove={(e) => draggingCursor(e)}
+              className="relative flex h-full w-full select-none flex-nowrap"
+            >
+              {/* final section */}
               <img
-                ref={wireFrameSection}
-                id="wireFrameSection"
                 src="/wireFrameFinal.jpg"
                 className="aspect-auto w-full max-w-none flex-shrink-0"
               />
-              <div className="absolute left-0 top-0 h-full w-1/2 flex-shrink-0 overflow-hidden bg-red-500">
+              {/* before section */}
+              <div
+                style={{
+                  width: `${cursorPositionPercentage * 100}%`,
+                }}
+                className="absolute left-0 top-0 h-full flex-shrink-0 overflow-hidden"
+              >
                 <img
                   src="/wireFrameBefore.jpg"
                   className={`absolute bottom-0 left-0 aspect-auto h-full w-[calc(100vw-144px)] max-w-none`}
                 />
-                <div className="absolute right-0 top-0 h-full w-[20px] translate-x-1/2 cursor-col-resize">
+                {/* Dragging section */}
+                <div
+                  onMouseDown={() => {
+                    setMouseDownActive(true);
+                  }}
+                  className="absolute right-0 top-0 h-full w-[20px] translate-x-1/2 cursor-col-resize"
+                >
                   <div className="absolute left-1/2 h-full w-[2px] -translate-x-1/2 bg-black"></div>
                   <div className="absolute left-1/2 top-1/2 flex w-max -translate-x-1/2 -translate-y-1/2 gap-x-[8px]">
                     <img
                       src={"/wireFrameArrow.svg"}
                       alt="arrow left"
                       loading="lazy"
-                      className="aspect-auto w-[24px]"
+                      className="pointer-events-none aspect-auto w-[24px]"
                     />
                     <img
                       src={"/wireFrameArrow.svg"}
                       alt="arrow right"
                       loading="lazy"
-                      className="aspect-auto w-[24px] rotate-180"
+                      className="pointer-events-none aspect-auto w-[24px] rotate-180"
                     />
                   </div>
                 </div>
