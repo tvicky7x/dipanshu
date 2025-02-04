ccdc2a6bcea98ba3d496fcd0970a6ef87326f788
tvicky7x <tvicky7x@gmail.com>
2025-02-05 00:03:48 +0530

    feat: design process done

--- src/components/DesignProcessSection/DesignProcessSection.js
+++ src/components/DesignProcessSection/DesignProcessSection.js
@@ -41,14 +41,17 @@ function DesignProcessSection() {
       <div className="relative h-[15vh]">
         <GridLines />
       </div>
-      <div ref={designProcessSectionRef} className="relative h-[4000px]">
+      <div
+        ref={designProcessSectionRef}
+        className="relative h-[2000px] lg:h-[3000px] xl:h-[4000px]"
+      >
         <div ref={pinnedDesignedRef} className="h-screen px-[72px]">
           <div className="flex h-full flex-col border-x-2 border-white/15">
             {/* heading Section */}
             <div className="relative h-[40vh] flex-shrink-0">
               {/* Heading text */}
               <h2
-                className={`${layGrotesk.className} absolute left-0 top-1/2 -translate-y-1/2 text-[60px] font-medium uppercase leading-normal tracking-[0.04em] xl:text-[70px]`}
+                className={`${layGrotesk.className} absolute left-0 top-1/2 -translate-y-1/2 text-[50px] font-medium uppercase leading-normal tracking-[0.04em] lg:text-[60px] xl:text-[70px]`}
               >
                 Lorem Ipsum
               </h2>
@@ -62,9 +65,9 @@ function DesignProcessSection() {
                     return (
                       <div key={index} className={`flex items-center`}>
                         {index === array.length - 1 && (
-                          <div className="flex w-full items-center justify-between border-y-2 border-white/15 px-[21px] py-[21px] xl:px-[24px] xl:py-[24px]">
+                          <div className="flex w-full items-center justify-between border-y-2 border-white/15 px-[18px] py-[18px] lg:px-[21px] lg:py-[21px] xl:px-[24px] xl:py-[24px]">
                             <span
-                              className={`${aeonikTrial.className} text-[15px] leading-[20px] xl:text-[17.43px] xl:leading-[23px]`}
+                              className={`${aeonikTrial.className} text-[13px] leading-[17.5px] lg:text-[15px] lg:leading-[20px] xl:text-[17.43px] xl:leading-[23px]`}
                             >
                               Lorem Ipsum
                             </span>
@@ -74,7 +77,7 @@ function DesignProcessSection() {
                               width={24}
                               height={24}
                               loading="lazy"
-                              className="aspect-auto w-[21px] xl:w-[24px]"
+                              className="aspect-auto w-[18px] lg:w-[21px] xl:w-[24px]"
                             />
                           </div>
                         )}
@@ -97,14 +100,14 @@ function DesignProcessSection() {
                             id="processContainer"
                             className={`${index === 0 && "bg-white"} flex h-full w-0 flex-col justify-center gap-y-[14px]`}
                           >
-                            <div className="mx-[16px] mb-[18px] mt-[16px] xl:mx-[20px] xl:mb-[22px] xl:mt-[20px]">
+                            <div className="mx-[14px] lg:mx-[16px] xl:mx-[20px]">
                               <p
-                                className={`${layGrotesk.className} line-clamp-1 text-[17px] leading-[29px] xl:text-[19.9px] xl:leading-[31px]`}
+                                className={`${layGrotesk.className} line-clamp-1 text-[15px] leading-[25px] lg:text-[17px] lg:leading-[29px] xl:text-[19.9px] xl:leading-[31px]`}
                               >
                                 Discover
                               </p>
                               <p
-                                className={`${aeonikTrial.className} line-clamp-2 text-[14px] leading-[20px] text-black text-opacity-50 xl:text-[15px] xl:leading-[22px]`}
+                                className={`${aeonikTrial.className} line-clamp-2 text-[13px] leading-[18px] text-black text-opacity-50 lg:text-[14px] lg:leading-[20px] xl:text-[15px] xl:leading-[22px]`}
                               >
                                 Estrutura inicial, mapeando funcionalidade e
                                 fluxo do projeto, sem detalhes visuais.
@@ -137,14 +140,14 @@ function DesignProcessSection() {
                             id="processContainer"
                             className={`flex h-full w-0 flex-col justify-center gap-y-[14px] bg-white`}
                           >
-                            <div className="mx-[16px] mb-[18px] mt-[16px] xl:mx-[20px] xl:mb-[22px] xl:mt-[20px]">
+                            <div className="mx-[14px] lg:mx-[16px] xl:mx-[20px]">
                               <p
-                                className={`${layGrotesk.className} line-clamp-1 text-[17px] leading-[29px] xl:text-[19.9px] xl:leading-[31px]`}
+                                className={`${layGrotesk.className} line-clamp-1 text-[15px] leading-[25px] lg:text-[17px] lg:leading-[29px] xl:text-[19.9px] xl:leading-[31px]`}
                               >
                                 Ideate and Conceptualize
                               </p>
                               <p
-                                className={`${aeonikTrial.className} line-clamp-2 text-[14px] leading-[20px] text-black text-opacity-50 xl:text-[15px] xl:leading-[22px]`}
+                                className={`${aeonikTrial.className} line-clamp-2 text-[13px] leading-[18px] text-black text-opacity-50 lg:text-[14px] lg:leading-[20px] xl:text-[15px] xl:leading-[22px]`}
                               >
                                 Nossa essência está em criar experiências
                                 visuais impactantes, combinando estética e
@@ -177,14 +180,14 @@ function DesignProcessSection() {
                             id="processContainer"
                             className={`${index === 3 && "bg-white"} flex h-full w-0 flex-col justify-center gap-y-[14px]`}
                           >
-                            <div className="mx-[16px] mb-[18px] mt-[16px] xl:mx-[20px] xl:mb-[22px] xl:mt-[20px]">
+                            <div className="mx-[14px] lg:mx-[16px] xl:mx-[20px]">
                               <p
-                                className={`${layGrotesk.className} line-clamp-1 text-[17px] leading-[29px] xl:text-[19.9px] xl:leading-[31px]`}
+                                className={`${layGrotesk.className} line-clamp-1 text-[15px] leading-[25px] lg:text-[17px] lg:leading-[29px] xl:text-[19.9px] xl:leading-[31px]`}
                               >
                                 Design and Refine
                               </p>
                               <p
-                                className={`${aeonikTrial.className} line-clamp-2 text-[14px] leading-[20px] text-black text-opacity-50 xl:text-[15px] xl:leading-[22px]`}
+                                className={`${aeonikTrial.className} line-clamp-2 text-[13px] leading-[18px] text-black text-opacity-50 lg:text-[14px] lg:leading-[20px] xl:text-[15px] xl:leading-[22px]`}
                               >
                                 Transformação do design em uma experiência
                                 interativa, com animações e funcionalidades
